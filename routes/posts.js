c373e77a76f32f5b38346061134229416390f926
Alejandro <aleguerra05@gmail.com>
2018-08-31 09:58:15 -0400

    adding new post form an smaller images

--- routes/posts.js
+++ routes/posts.js
@@ -6,12 +6,13 @@ var Client = require('node-rest-client').Client;
 var client = new Client();
 
 client.registerMethod("getPosts", "http://localhost:3001/posts/${id}", "GET");
+client.registerMethod("createPosts", "http://localhost:3001/posts/", "POST");
 client.registerMethod("getComments", "http://localhost:3001/posts/${id}/comments", "GET");
 client.registerMethod("getImages", "http://localhost:3001/posts/${id}/images", "GET");
 //client.registerMethod("getAllPosts", "http://localhost:3001/posts/", "GET");
 
 router.get('/', function (req, res, next) {
-    
+
     client.get("http://localhost:3001/posts/", function (data, response) {
         console.log(response.statusCode);
         if (response.statusCode == 200) {
@@ -21,7 +22,37 @@ router.get('/', function (req, res, next) {
             res.render('message', { title: 'Error', message: 'Post count: ' + response.statusMessage });
         }
     });
-    
+
+});
+
+router.post('/', function (req, res, next) {
+    console.log(req.body);
+
+    if (req.body != null&&req.body.title != null && req.body.text != null && req.body.author != null) {
+        var args = {
+            data: { title: req.body.title, author: req.body.author, text: req.body.text },
+            headers: { "Content-Type": "application/json" }
+        };
+
+        client.methods.createPosts(args, function (data, response) {
+
+            if (response.statusCode == 201) {
+                client.get("http://localhost:3001/posts/", function (data, response) {
+                    console.log(response.statusCode);
+                    if (response.statusCode == 200) {
+                        res.render('post_list', { title: 'Added post!', posts: data });
+                    }
+                    else {
+                        res.render('message', { title: 'Error', message: 'Post count: ' + response.statusMessage });
+                    }
+                });
+            }
+            else
+                res.render('message', { title: 'Error', message: 'Error ading post!' + response.statusMessage });
+        });
+    }
+    else
+        res.render('message', { title: 'Error', message: 'Error ading post! ParamsRequired'});        
 });
 
 router.get('/:postId(\\d+)', function (req, res, next) {
@@ -36,9 +67,9 @@ router.get('/:postId(\\d+)', function (req, res, next) {
         console.log(response.statusCode);
         if (response.statusCode == 200) {
 
-            var args ={path:{"id":req.params.postId}};
+            var args = { path: { "id": req.params.postId } };
 
-            client.methods.getComments(args,function (comments, commentsResponse) {
+            client.methods.getComments(args, function (comments, commentsResponse) {
                 console.log(comments);
                 console.log(commentsResponse.statusCode);
 
@@ -46,7 +77,7 @@ router.get('/:postId(\\d+)', function (req, res, next) {
                     console.log(images);
                     console.log(imagesResponse.statusCode);
 
-                    res.render('post', { title: 'Post ' + req.params.postId, post: data, comments: comments, images:images });
+                    res.render('post', { title: 'Post ' + req.params.postId, post: data, comments: comments, images: images });
                 });
             });
         }
@@ -57,20 +88,6 @@ router.get('/:postId(\\d+)', function (req, res, next) {
 });
 
 router.get('/count', function (req, res, next) {
-
-    // var args = null;
-
-    // client.methods.getAllPosts(args, function (data, response) {
-    //     //console.log(data);
-    //     console.log(response.statusCode);
-    //     if (response.statusCode == 200) {
-    //         res.render('message', { title: 'Posts Count ', message: 'PostCount: ' + response.lenght });
-    //     }
-    //     else {
-    //         res.render('message', { title: 'Error', message: 'Post count: ' + response.statusMessage });
-    //     }
-    // });
-
     client.get("http://localhost:3001/posts/", function (data, response) {
         console.log(response.statusCode);
         if (response.statusCode == 200) {
@@ -80,7 +97,7 @@ router.get('/count', function (req, res, next) {
             res.render('message', { title: 'Error', message: 'Post count: ' + response.statusMessage });
         }
     });
-
 });
+
 
 module.exports = router;
