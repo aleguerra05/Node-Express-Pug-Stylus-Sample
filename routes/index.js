99b1ea8e95d44019d9cca5e2535b860de9c976c6
Alejandro <aleguerra05@gmail.com>
2019-01-12 10:40:15 -0500

    adding personalization

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Lugar', subtitle: 'Subtítulo' });
+  res.render('index', { title: 'Palacio Segundo Cabo', subtitle: 'Subtítulo' });
 });
 
 module.exports = router;
