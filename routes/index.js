93ef7eabf2441453d294ec25d28973df2f841277
Alejandro <aleguerra05@gmail.com>
2019-01-16 23:42:48 -0500

    search and style changes

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,11 @@ var fs = require('fs');
 var Client = require('node-rest-client').Client;
 var client = new Client();
 
+client.registerMethod("search", "http://localhost:3001/posts?code=${codeValue}", "GET");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Palacio Segundo Cabo', subtitle: 'Subtítulo' });
-
+  
   client.get("http://localhost:3001/posts/", function (data, response) {
       console.log(response.statusCode);
       if (response.statusCode == 200) {
@@ -18,6 +19,22 @@ router.get('/', function(req, res, next) {
           res.render('message', { title: 'Error', message: 'Cantidad de Contenidos: ' + response.statusMessage });
       }
   });
+});
+
+router.post('/',function(req,res,next){
+    var args = {
+        path: { "codeValue": req.body.codeValue }
+    }
+
+    client.methods.search(args, function (data, response) {
+        console.log(response.statusCode);
+        if (response.statusCode == 200) {
+            res.render('index', { title: 'Lista de Contenidos ', posts: data });
+        }
+        else {
+            res.render('message', { title: 'Error', message: 'Cantidad de Contenidos: ' + response.statusMessage });
+        }
+    });
 });
 
 module.exports = router;
