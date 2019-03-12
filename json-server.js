4b7c21707e909d52a89fb59ddd715c0b4cc90303
Alejandro <aleguerra05@gmail.com>
2019-03-12 14:43:55 -0400

    adding imageSize to 3001

--- json-server.js
+++ json-server.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var fs = require('fs');
 
 const jsonServer = require('json-server')
 const server = jsonServer.create()
@@ -8,6 +9,38 @@ console.log(path.join(__dirname,'public', dbFile));
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
+
+
+
+server.get('/images/allSize',function(req,res,next){
+
+  var imagePath = 'public/images/';
+
+  var files = fs.readdir(imagePath,function(err,files){
+      //console.log(files);
+      //console.log(err);
+      var total_size = 0;
+
+      files.forEach(f => {
+          
+          var stat = fs.lstatSync(path.join(imagePath,f));
+          total_size += stat.size;
+          //console.log(stat.size);
+          
+          /*
+          console.log(path.join(imagePath,f));
+          var stats = fs.statSync(imagePath,f);
+          console.log(stats);
+          var size = fs.statSync(imagePath,f)["size"];
+          console.log(size);
+          */
+      });
+      console.log(total_size);
+      res.setHeader('Content-Type','application/json');
+      res.send(JSON.stringify({total_size:total_size/1024/1024}));
+     
+  });
+});
 
 server.use(router)
 server.listen(3001, () => {
